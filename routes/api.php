1902cd1a6652b17300b331635e383ca4e07503a2
fatiha098 <chehaibfatiha@gmail.com>
2025-02-19 11:07:41 +0100

    column genre added

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,6 @@ use App\Http\Controllers\NotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::apiResource('employes', EmployeController::class);
 Route::apiResource('notifications', NotificationController::class)->middleware('auth:sanctum');
